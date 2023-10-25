eece5a67940966d67a5f1675ab17e3f6a7c61ee3
Adam R. Scroggin <adam.scroggin@lftsoftware.co>
2023-10-25 16:25:22 -0400

    interim check in

--- Program.cs
+++ Program.cs
@@ -1,15 +1,50 @@
-﻿namespace HelloWorld
+﻿using System.Threading;
+
+
+namespace HelloWorld
 {
   class Program
   {
     static void Main(string[] args)
     {
-      Console.WriteLine("What is your name?");
       var name = Console.ReadLine();
       var currentDate = DateTime.Now;
-      Console.WriteLine($"{Environment.NewLine}Hello, {name}, on {currentDate:d} at {currentDate:t}!");
+      WriteTerminal($"{Environment.NewLine}Greetings {name}, on {currentDate:d} at {currentDate:t}!\n");
+
+      Console.WriteLine("Would you like to play a game?");
+      var answer = Console.ReadLine();
+
+      PrintGames();
+      ExitProgram();
+
+    }
+
+
+    private static void ExitProgram()
+    {
       Console.Write($"{Environment.NewLine}Press any key to exit...");
       Console.ReadKey(true);
+    }
+
+    private static void PrintGames()
+    {
+      Console.WriteLine("Chess");
+      Console.WriteLine("Poker");
+      Console.WriteLine("Fighter Combat");
+      Console.WriteLine("Guerrilla Engagement");
+      Console.WriteLine("Desert Warfare");
+
+      Console.WriteLine("\nGlobal Thermonuclear War");
+
+    }
+
+    private static void WriteTerminal(string outString)
+    {
+      foreach (char c in outString)
+      {
+          Console.Write(c);
+          Thread.Sleep(140);
+      }
     }
   }
 }
